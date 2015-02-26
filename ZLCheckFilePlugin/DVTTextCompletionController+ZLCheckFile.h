75d2d96bd96dfea2be6d8cf7c43525e9c4dccaba
zhangleo <zhangleowork@163.com>
2015-02-26 21:22:33 +0800

    add open directory file path.

---
+++ ZLCheckFilePlugin/DVTTextCompletionController+ZLCheckFile.h
@@ -0,0 +1,13 @@
+//
+//  DVTTextCompletionController+ZLCheckFile.h
+//  ZLCheckFilePlugin
+//
+//  Created by 张磊 on 15-1-21.
+//  Copyright (c) 2015年 com.zixue101.www. All rights reserved.
+//
+
+#import "DVTTextCompletionController.h"
+
+@interface DVTTextCompletionController (ZLCheckFile)
+
+@end
---
+++ ZLCheckFilePlugin/DVTTextCompletionController.h
@@ -0,0 +1,18 @@
+//
+//  DVTTextCompletionController.h
+//  ZLCheckFilePlugin
+//
+//  Created by 张磊 on 15-1-21.
+//  Copyright (c) 2015年 com.zixue101.www. All rights reserved.
+//
+
+//#ifndef ZLCheckFilePlugin_DVTTextCompletionController_h
+//#define ZLCheckFilePlugin_DVTTextCompletionController_h
+
+#import <Cocoa/Cocoa.h>
+
+@interface DVTTextCompletionController : NSObject
+- (BOOL)acceptCurrentCompletion;
+@end
+
+//#endif
